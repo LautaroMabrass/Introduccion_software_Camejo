0496bb0ad481eb7877469910c1d72c7ceef2fcfb
Lautaro Mabras <mabraslautaroezequiel@hotmail.com>
2025-03-19 20:21:16 -0300

    ej 1-4

---
+++ bash_ej4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mayor_menor_igual(){
+    read -p "Ingresa un numero y verificare si es >,<,= a 0: " n
+    if [[ ! $n =~ ^-?[0-9]+$ ]]; then
+        echo "ERROR! no es un numero"
+    elif [[ $n -gt 0 ]]; then
+        echo "Es mayor a 0"
+    elif [[ $n -lt 0 ]]; then
+        echo "Es menor a 0"
+    else
+        echo "Es igual a 0"
+    fi
+}
+
+mayor_menor_igual
